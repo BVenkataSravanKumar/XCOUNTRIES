9b465f76335e482660586a530d8354c01cdcc064
VENKAT <131847253+BVENKATASRAVANAKUMAR@users.noreply.github.com>
2025-08-07 12:50:00 +0530

    Updated app

--- countries/src/App.js
+++ countries/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect,useState} from "react";
+import "./App.css";
 
-function App() {
+function App(){
+  const [countries, setCountries]=useState([]);
+
+  useEffect(()=>{
+    const fectchCountries= async ()=>{
+      try{
+        const response= await fetch('https://xcountries-backend.azurewebsites.net/all');
+        if(!response.ok){
+          throw new Error(`${response.status}`);
+        }
+        const data= await response.json();
+        setCountries(data);
+      }catch(error){
+        console.error('Error fetching data:', error);
+      }
+    };
+    fectchCountries();
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {countries.map((country, index) => (
+        <div className="card" key={index}>
+          <img
+            src={country.flag}
+            alt={`Flag of ${country.name}`}
+            className="flag"
+          />
+          <p className="name">{country.name}</p>
+        </div>
+      ))}
     </div>
   );
 }
